c8e4cdc2ab9bc856d75fd889bf1f0534eb42b569
simonzyh <525064605@qq.com>
2016-07-07 09:55:48 +0800

    format code

--- src/main/java/forkjoin/Test.java
+++ src/main/java/forkjoin/Test.java
@@ -10,20 +10,18 @@ import java.util.concurrent.Future;
 public class Test {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-
         ForkJoinPool forkJoinPool = new ForkJoinPool(50);
-
         //生成一个计算任务，负责计算1+2+3+4
         Long l1 = System.currentTimeMillis();
         CountTask task = new CountTask(1, 4000000000L);
         task.fork();
         //执行一个任务
-
         System.out.println(task.join() + " " + (System.currentTimeMillis() - l1));
+
+
         CountTask task2 = new CountTask(1, 4000000000L);
         Long l2 = System.currentTimeMillis();
         Future result = forkJoinPool.submit(task2);
         System.out.println(result.get() + " " + (System.currentTimeMillis() - l2));
-
     }
 }
