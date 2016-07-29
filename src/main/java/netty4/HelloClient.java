a6f1e9e28f987ad551b7e7174b4b636e648101cb
simonzyh <525064605@qq.com>
2016-07-29 17:44:09 +0800

    format code

--- src/main/java/netty4/HelloClient.java
+++ src/main/java/netty4/HelloClient.java
@@ -39,12 +39,16 @@ public class HelloClient {
                 if (line == null) {
                     continue;
                 }
+                if("exit".equals(line)){
+                    ch.close();
+                    break;
+                 }
                 /*
                  * 向服务端发送在控制台输入的文本 并用"\r\n"结尾
                  * 之所以用\r\n结尾 是因为我们在handler中添加了 DelimiterBasedFrameDecoder 帧解码。
                  * 这个解码器是一个根据\n符号位分隔符的解码器。所以每条消息的最后必须加上\n否则无法识别和解码
                  * */
-                ch.writeAndFlush(line + "\r\n");
+                ch.writeAndFlush(line + "\n");
             }
         } finally {
             // The connection is closed automatically on shutdown.
