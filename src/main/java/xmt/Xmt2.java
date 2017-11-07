3aaead648d22e7454fa353d09d085beef7ef0bbc
yehua.zyh <yehua.zyh@alibaba-inc.com>
2017-11-07 15:26:54 +0800

    format code

--- src/main/java/xmt/Xmt2.java
+++ src/main/java/xmt/Xmt2.java
@@ -9,7 +9,7 @@ public class Xmt2 {
     public static void main(String[] args) throws InterruptedException {
         System.out.println( Xmt2.class+" atart");
 
-        Thread.sleep(2*60*1000);
+        Thread.sleep(2*60* 1000);
 
         System.out.println( Xmt2.class+" end");
     }
