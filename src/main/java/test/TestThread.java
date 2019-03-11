e5ef38b047866c1a55c9af9271bb094d97512d7d
yhzhang <git12345678>
2019-03-11 18:01:21 +0800

    bytebuddy

--- src/main/java/test/TestThread.java
+++ src/main/java/test/TestThread.java
@@ -4,48 +4,67 @@ public class TestThread {
     static Object obj = new Object();
 
     public static void main(String[] args) {
-        t1.start();
-        t2.start();
+        //t1.start();
+        //t2.start();
+        t3.start();
+        t4.start();
 
     }
 
-    static Thread t1 = new Thread() {
-        @Override
-        public void run() {
-            for (int i = 0; i < 10; i++) {
-                synchronized (obj) {
-                    obj.notify();
+    static Thread t1 = new Thread(() -> {
+        for (int i = 0; i < 10; i++) {
+            synchronized (obj) {
+                obj.notify();
 
-                    System.out.println("t1:" + i);
-                    try {
-                        obj.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    if(i==9){
-                        obj.notifyAll();
-                    }
+                System.out.println("t1:" + i);
+                try {
+                    obj.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-
+                if (i == 9) {
+                    obj.notifyAll();
+                }
             }
-        }
-    };
-    static Thread t2 = new Thread() {
-        @Override
-        public void run() {
-            for (int i = 0; i < 10; i++) {
-                synchronized (obj) {
-                    obj.notify();
 
-                    System.out.println("t2:" + i);
-                    try {
-                        obj.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+        }
+    });
+    static Thread t2 = new Thread(() -> {
+        for (int i = 0; i < 10; i++) {
+            synchronized (obj) {
+                obj.notify();
+
+                System.out.println("t2:" + i);
+                try {
+                    obj.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (i == 9) {
+                    obj.notifyAll();
                 }
             }
         }
-    };
+    });
+    static Thread t3 = new Thread(() -> {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getId() + " " + i);
+
+            Thread.currentThread().yield();
+
+        }
+    });
+
+    static Thread t4 = new Thread(() -> {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getId() + " " + i);
+
+            Thread.currentThread().yield();
+
+        }
+    });
+
+
+
 
 }
